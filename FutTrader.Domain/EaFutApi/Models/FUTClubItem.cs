57c800f49cac2af25bc96d4b73748b9d14a6aba8
matt s <matt_sage@hotmail.co.uk>
2020-03-21 18:10:45 +0000

    EaFut models

---
+++ FutTrader.Domain/EaFutApi/Models/FUTClubItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+using Newtonsoft.Json;
+
+
+namespace Crawler.Models
+{
+    public class FUTClubItem
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("abbrName")]
+        public string Abbreviation { get; set; }
+    }
+}
